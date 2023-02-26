4f553f8916ae51eb9f76f3c7021f5b586cd45ff5
松岡宏樹 <matsuokahiroki@matsuokahirokinoMacBook-Pro.local>
2023-02-26 13:52:33 +0900

    メリットカードのコンポーネント作成

--- src/components/MeritCard/MeritCard.tsx
+++ src/components/MeritCard/MeritCard.tsx
@@ -3,19 +3,16 @@ import styles from './index.module.scss';
 
 type Props = {
   imageUrl: string;
-  altName: string;
   text1: string;
   text2: string;
-  text3: string;
 };
 
-export const IntroductionCard: FC<Props> = ({ imageUrl, altName, text1, text2, text3 }) => {
+export const MeritCard: FC<Props> = ({ imageUrl, text1, text2 }) => {
   return (
     <div className={styles.container}>
-      <img src={imageUrl} alt={altName} className={styles.container__img} />
+      <img src={imageUrl} className={styles.container__img} />
       <div className={styles.container__text1}>{text1}</div>
       <div className={styles.container__text2}>{text2}</div>
-      <div className={styles.container__text3}>{text3}</div>
     </div>
   );
 };
