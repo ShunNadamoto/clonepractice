8386edeb8943bf02d538aca3f8b8b006d2e11cae
masaki-tanaka0417 <masamasamasa0417@gmail.com>
2023-03-04 01:10:04 +0900

    ああああ

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -3,7 +3,13 @@ import type { NextPage } from 'next';
 const Test: NextPage = () => {
   console.log('aaa');
 
-  return <div>新しいページ</div>;
+  return (
+    <div>
+      <div>新しいページ</div>
+      <div>aaaaa</div>
+      <div>reaefafad</div>
+    </div>
+  );
 };
 
 export default Test;
