75ecef7d98adb7490eae2fca47f1193776f11212
松岡宏樹 <matsuokahiroki@matsuokahirokinoMacBook-Pro.local>
2023-03-06 17:26:30 +0900

    吉野屋コンポーネント作成中

--- src/components/Yoshinoya/Yoshinoya.tsx
+++ src/components/Yoshinoya/Yoshinoya.tsx
@@ -6,15 +6,17 @@ type Props = {
   tag2: string;
   imgUrl: string;
   menu: string;
-  price: string;
+  price1: string;
+  price2: string;
 };
 
-export const Yoshinoya: FC<Props> = ({ tag1, tag2, imgUrl, menu, price }) => (
+export const Yoshinoya: FC<Props> = ({ tag1, tag2, imgUrl, menu, price1, price2 }) => (
   <div className={styles.content}>
     <div className={styles.content__tag1}>{tag1}</div>
     <div className={styles.content__tag2}>{tag2}</div>
-    <img className={styles.content__imgurl} src={imgUrl} />
+    <img className={styles.content__img} src={imgUrl} />
     <div className={styles.content__menu}>{menu}</div>
-    <div className={styles.content__price}>{price}</div>
+    <div className={styles.content__price1}>{price1}</div>
+    <div className={styles.content__price2}>{price2}</div>
   </div>
 );
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,12 +1,8 @@
 import type { NextPage } from 'next';
 import { Sukiya } from '../../components/Sukiya2';
+import { Yoshinoya } from '../../components/Yoshinoya';
 
 const Test: NextPage = () => {
-  console.log('aaa');
-  const caluculation = (a: number, b: number) => {
-    return a + b;
-  };
-  console.log(caluculation(1, 2));
   const caluculation2 = (a: number, b: number) => a - b;
   console.log(caluculation2(2, 1));
   const sum2 = (a: string, b: string) => {
@@ -14,16 +10,14 @@ const Test: NextPage = () => {
   };
   const sum３ = (a: string, b: string) => `${a}は${b}です。`;
   console.log(sum2('松岡', 'PC音痴'));
-  console.log(sum３('松岡', '不器用'));
+  console.log(sum2('千里さん', '優しい人'));
+  console.log(sum2('田中さん', '愛のある指導者'));
+  const sum = (a: string, b: string) => `${a}は${b}します。`;
+  console.log(sum('松岡', 'プログラミングの勉強を'));
 
-  const array = [1, 3, 5, 7, 9];
+  const array = [1, 2, 3];
   const newArray = array.map((elem) => elem * 2);
-  const array9 = [1, 2, 3, 4];
-  console.log(array);
   console.log(newArray);
-  const array2 = [2, 4, 6, 8, 10];
-  const newArray2 = array2.map((elem) => elem * 3);
-  console.log(newArray2);
 
   return (
     <div>
@@ -33,12 +27,49 @@ const Test: NextPage = () => {
       <Sukiya
         tagUrlList={[
           'https://www.sukiya.jp/menu/common/img/icon_menu_new.png',
-          'https://www.sukiya.jp/menu/common/img/icon_menu_new.png',
-          'https://www.sukiya.jp/menu/common/img/icon_menu_new.png',
+          'https://www.sukiya.jp/menu/common/img/icon_menu_limited_time.png',
+          'https://www.sukiya.jp/menu/common/img/icon_menu_recommend.png',
         ]}
-        menu='aa'
+        menu='麻辣もやし牛丼'
         imgUrl='https://www.sukiya.jp/menu/img/in/photo_gyudon_111400.jpg'
       />
+      <Sukiya
+        tagUrlList={['https://www.sukiya.jp/menu/common/img/icon_menu_recommend.png']}
+        menu='牛丼'
+        imgUrl='https://www.sukiya.jp/menu/img/in/photo_gyudon_100100.jpg'
+      />
+      <Sukiya
+        tagUrlList={[
+          'https://www.sukiya.jp/menu/common/img/icon_menu_new.png',
+          'https://www.sukiya.jp/menu/common/img/icon_menu_limited_time.png',
+          'https://www.sukiya.jp/menu/common/img/icon_menu_recommend.png',
+        ]}
+        menu='麻辣もやし牛丼'
+        imgUrl='https://www.sukiya.jp/menu/img/in/photo_gyudon_111400.jpg'
+      />
+      <Sukiya
+        tagUrlList={['https://www.sukiya.jp/menu/common/img/icon_menu_recommend.png']}
+        menu='牛丼'
+        imgUrl='https://www.sukiya.jp/menu/img/in/photo_gyudon_100100.jpg'
+      />
+      <div>
+        <Yoshinoya
+          tag1='NEW'
+          tag2='期間限定'
+          imgUrl='https://www.yoshinoya.com/wp-content/uploads/2023/02/24201838/teppan_gyu_yakiniku_teisyoku.jpg'
+          menu='鉄板牛焼肉定食'
+          price1='648円'
+          price2='（税込712円)'
+        />
+        <Yoshinoya
+          tag1='NEW'
+          tag2='期間限定'
+          imgUrl='https://www.yoshinoya.com/wp-content/uploads/2023/02/24201839/teppan_gyu_karubi_teisyoku-1.jpg'
+          menu='鉄板牛カルビ定食'
+          price1='648円'
+          price2='（税込712円)'
+        />
+      </div>
     </div>
   );
 };
