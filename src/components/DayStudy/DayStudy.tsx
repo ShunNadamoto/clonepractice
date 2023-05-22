3982303b4189501cb6fd56f62cbbdc43c853374c
hirokimatsuoka <celica24ss3@gmail.com>
2023-05-23 00:51:10 +0900

    あああ

--- src/components/DayStudy/DayStudy.tsx
+++ src/components/DayStudy/DayStudy.tsx
@@ -69,6 +69,23 @@ const persons = {
 };
 console.log(persons.Yamada.weight);
 
+const person3 = {
+  name: 'aaa',
+  age: 20,
+};
+const person4 = { name: 'ccc', age: 2 };
+const person5 = { name: 'ddd', gender: '男性' };
+const person6 = { height: 1.8, weight: 70 };
+const apple = { name: 'apple', color: 'red' };
+const rice = { name: '米', color: '白' };
+const dog = { name: 'taro', type: '犬' };
+console.log(person6.height);
+const personList6 = [
+  { name: '太郎', age: 5, note: '子供', registerDate: '20230519' },
+  { name: '二郎', age: 13, note: '中学生', registerDate: '20230524' },
+  { name: '三郎', age: 7, note: '小学生', registerDate: '20230522' },
+];
+
 type Props = {
   testName: string;
   testAge: number;
