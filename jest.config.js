d05baf06ee7c1919c35f41ca0524dde3eb1ab646
hirokimatsuoka <celica24ss3@gmail.com>
2023-07-24 10:45:55 +0900

    useContext練習

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ const createJestConfig = nextJest({ dir: './' });
 const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js | jsx | ts | tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+
   testEnvironment: 'jsdom',
 };
 
