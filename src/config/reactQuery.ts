5f847e7bea2b4fba72afe578f182fc022c5bba6a
hirokimatsuoka <celica24ss3@gmail.com>
2023-11-15 11:25:02 +0900

    axiosのget,post発動練習

--- src/config/reactQuery.ts
+++ src/config/reactQuery.ts
@@ -2,11 +2,6 @@ import { QueryClient } from 'react-query';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {
-      cacheTime: 300000, // クエリのキャッシュ有効期限（ミリ秒）
-      staleTime: 60000, // クエリがステールとみなされるまでの時間（ミリ秒）
-      refetchOnWindowFocus: false, // ウィンドウフォーカス時にクエリを再実行するかどうか
-    },
     mutations: {
       onError: (error: any) => {
         //ミューテーションがエラーの場合の処理
