ad8f54af55617e1bd9961c55c7426f532762b468
masaki-tanaka0417 <masamasamasa0417@gmail.com>
2024-01-04 01:08:17 +0900

    環境変数にAPIの向き先を分離した

--- src/lib/axiosInstance2.ts
+++ src/lib/axiosInstance2.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://shop-ivx3.onrender.com',
+  baseURL: process.env.NEXT_PUBLIC_API_URL2,
 });
 
 export default axiosInstance;
